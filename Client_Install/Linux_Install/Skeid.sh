7a29ba47b37625efd3dc3a66aa2405b7690141e9
Maninthebath <108490276+Maninthebath@users.noreply.github.com>
2022-11-01 22:14:17 -0600

    Update Skeid.sh

--- Client_Install/Linux_Install/Skeid.sh
+++ Client_Install/Linux_Install/Skeid.sh
@@ -10,6 +10,7 @@ then
     sudo apt install python3-pip
     sudo apt install wget
     cd /home/kali/Desktop
+    sudo pip install python-crontab
     wget https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Urd.py
     chmod +x Urd.py
     ./Urd.py
