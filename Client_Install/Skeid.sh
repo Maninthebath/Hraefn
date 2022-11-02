ffd3a09d8a7f93b32e2b5fdc4ce533f0f4ad1b12
Toni Mason <115134681+lilbigyell@users.noreply.github.com>
2022-11-01 17:34:32 -0700

    Create Skeid.sh
    Linux Client install sh file

---
+++ Client_Install/Skeid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$(lsb_release -a)
+
+KALI='Kali'
+
+if [[ "$VERSION" == *"$KALI"* ]];
+then
+    sudo apt install python3.6
+    sudo apt install python3-pip
+    sudo apt install wget
+    cd /home/kali/Desktop
+    wget https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Urd.py
+    chmod +x cc.py
+    ./cc.py
+fi
