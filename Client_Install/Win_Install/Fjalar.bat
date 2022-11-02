32ab87f6ae413898bad3a7c64f60e5a0d4be3392
Toni Mason <115134681+lilbigyell@users.noreply.github.com>
2022-11-01 18:24:30 -0700

    Update Fjalar.bat

--- Client_Install/Win_Install/Fjalar.bat
+++ Client_Install/Win_Install/Fjalar.bat
@@ -5,6 +5,6 @@ cd Pro*
 curl https://www.python.org/ftp/python/3.10.8/python-3.10.8-amd64.exe -o python-3.10.8-amd64.exe
 python-3.10.8-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 curl -LJO https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Urd.py
-curl -LJO https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Urd_batstart.vbs
-curl -LJO https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Urdstart.bat
+curl -LJO https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Win_Install/Urd_batstart.vbs
+curl -LJO https://github.com/Maninthebath/Hraefn/raw/main/Client_Install/Win_Install/Urdstart.bat
 schtasks /CREATE /TN "Hraefn" /TR "cscript.exe '%LocalAppData%\Programs\Urd_batstart.vbs'" /SC ONLOGON /IT
